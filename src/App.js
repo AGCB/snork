d7f91968f87482a1bdb62bee8cb4bf84f49a68b1
alexClifton <oooalexoooo@gmail.com>
2020-11-14 16:16:12 -0800

    merge grid-focus into master. single cell movement works now

--- src/App.js
+++ src/App.js
@@ -1,86 +1,113 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
-function App() {
+function setInitialGrid(size) {
+  let output = [];
+  for(let x = 0; x < size; x++) {
+    let col = [];
+    for (let y = 0; y < size; y++) {
+      col.push({
+        x,
+        y,
+        active: false,
+        hasFood: false,
+      })
+    }
+    output.push(col);
+  }
+  return output;
+}
 
-    // which way is snork facing?
-    let [direction, setDirection ] = useState('right');
-    //
-    // snork is an array of 2element arrays representing X/Y coords.
-    // snork starts with a length of 2
-    let [ snork, setSnork ] = useState([[0,0], [10.5,0]])
-    //
-    //
-    let [ incrementer, setIncrementer ] = useState(0) // for fun.
-
-    // snork listens for up/down/left/right keys
-    useEffect(() => {
-      const handleKeyPress = (e) => {
-        if ( e.key === 'ArrowLeft') {
-          console.log('left arrow');
-          setDirection('left');
-        }
-        if ( e.key === 'ArrowRight') {
-          console.log('right arrow');
-          setDirection('right');
-        }
-        if ( e.key === 'ArrowUp') {
-          console.log('up arrow');
-          setDirection('up');
-        }
-        if ( e.key === 'ArrowDown') {
-          console.log('down arrow');
-          setDirection('down');
-        }
-      }
-      window.addEventListener('keydown', handleKeyPress);
-      return () => window.removeEventListener('keydown', handleKeyPress);
-    })
-
-
-    useEffect(()=> {
-      const offsetMap = {
-        "right": [15,0],
-        "up": [0, -15],
-        "down": [0, 15],
-        "left": [-15, 0],
-      };
-      const snorkInterval = setInterval(()=>{
-        console.log('we are in an interval');
-        // ... to log the incrementer.
-        setIncrementer(incrementer++)
-
-        // currentOffset is nice since I don't have to
-        // create individual if()'s for up/down/left/right.
-        //
-        let currentOffset = offsetMap[direction];
-        let segments = snork.map(seg => {
-          return [seg[0]+currentOffset[0], seg[1]+currentOffset[1]]
-        });
-        setSnork(segments);
-
-      },150);
-      return () => clearInterval(snorkInterval);
-    },[snork])
+function Square({ squareData, snork, setSnork }) {
+  const { active, hasFood, x, y } = squareData;
 
   return (
-    <div className="App">
-      <h1>snork</h1>
-      <h2>snork stats</h2>
-      <p className="incrementer">{incrementer}</p>
-      <div className="snork">
+    <div
+      className={classNames({
+        'square': true,
+        'active': x === snork[0] && y === snork[1],
+        'food': hasFood,
+        })
+      }
+      onClick={e => {
+        console.log(squareData);
+      }}
+      >
+    </div>
+  )
+}
+
+function Column({columnData, snork, setSnork}) {
+  return (
+    <div className="column">
+      {
+        columnData.map((row,i) => {
+          return (
+            <Square
+              key={i}
+              squareData={row}
+              snork={snork}
+              setSnork={setSnork}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+function App() {
+  const SIZE = 10;
+  let [ grid, setGrid ] = useState(setInitialGrid(SIZE));
+  let [ snork, setSnork ] = useState([0,0]);
+  let [ errorState, setErrorState ] = useState(false);
+
+  useEffect(()=>{
+    function handleKeyPress(e) {
+      console.log('in a key press');
+      const offsets = {
+        'ArrowUp': [0, -1],
+        'ArrowRight': [1, 0],
+        'ArrowLeft': [-1, 0],
+        'ArrowDown': [0, 1],
+      };
+      setSnork([snork[0] + offsets[e.key][0],snork[1] + offsets[e.key][1]])
+      if(!errorState) {
+        if(snork[0] < 0 ||
+          snork[0] > SIZE-1 ||
+          snork[1] < 0 ||
+          snork[1] > SIZE-1) {
+            setErrorState(true)
+          }
+      }
+
+    }
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+
+  },[snork]);
+
+  if(errorState) {
+    return <span style={{"color":"red"}}>LOSE</span>
+  }
+  return (
+
+    <div
+      className="App"
+    >
+      <h1 className="title">snork</h1>
+      <div className="grid">
         {
-          snork.map(segment => {
+          grid.map((col,i) => {
             return (
-              <div
-                className="segment"
-                style={{"position": "absolute",
-                  "left": String(segment[0])+"px",
-                  "top": String(segment[1])+"px",
-                  "width": "10px",
-                  "height": "10px",
-                }}>
-              </div>
+              <Column
+                key={i}
+                columnData={col}
+                snork={snork}
+                setSnork={setSnork}
+              />
             )
           })
         }
